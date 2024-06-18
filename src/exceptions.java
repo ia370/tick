5e4c804a20ae592321a767b0b5604b8d4e89d353
issaaaly <ia370@sussex.ac.uk>
2024-06-18 21:18:13 +0100

    tareq

---
+++ src/exceptions.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class exceptions {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("book");
+        System.out.println(list.get(0));
+        // String[] pets = {"dog", "cat", "monkey"};
+        //System.out.println(pets[3]);
+    }
+}
---
+++ src/tryCatchExamples.java
@@ -0,0 +1,15 @@
+ public class tryCatchExamples {
+    public static void main(String[] args) {
+
+
+
+
+
+        //try{
+         //   int[] a = {4,5,1};
+           // System.out.println(a[3]);
+        //} catch(Exception e) {
+          //  System.out.println("an exception happened");
+        }
+    }
+
